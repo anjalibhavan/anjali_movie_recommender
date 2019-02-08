704a83fbaa92c5c4e66db5bfa146ee94976fa933
Anjali Bhavan <anjalibhavan98@gmail.com>
2019-02-09 01:52:00 +0530

    changed filename

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-#from flask_thumbnails import Thumbnail
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-#<!--<li display='inline'>{{ row.Index, row['name'] }}<img src="{{row['poster']}}" style="width:40px;height:40px;"> </li>-->
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
